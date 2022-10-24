a7a604ef7e37492d5a83d309abc9b97e44f5521d
Mohith Reddy <dev.m0hithreddy@gmail.com>
2022-10-25 01:48:27 +0530

    Fix comment string in task_executed()

--- djag_scheduler/tasks.py
+++ djag_scheduler/tasks.py
@@ -7,7 +7,7 @@ from djag_scheduler.event_queue import DjagEventQueue
 
 @task_postrun.connect
 def task_executed(task_id, state, *args, **kwargs):
-    """Set executed task ids in django cache"""
+    """Send task post execution event"""
     DjagEventQueue.put({
         'event': 'TASK_EXECUTED',
         'task_id': task_id,
